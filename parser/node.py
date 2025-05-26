14d26755e37f82578f9058d24c2c1f8cc72a7de3
rafaelvenetikides <rafavenetikides@gmail.com>
2025-05-26 15:22:37 -0300

    Table 2.0

--- parser/node.py
+++ parser/node.py
@@ -1,11 +1,18 @@
 class ASTNode:
-    def __init__(self, type_, value = None, children = None):
+    def __init__(self, type_, value=None, children=None):
         self.type = type_
         self.value = value
         self.children = children or []
 
-    def __repr__(self, level = 0):
-        ret = " " * level + f"{self.type}: {self.value}\n"
-        for child in self.children:
-            ret += child.__repr__(level + 1)
-        return ret
+    def __repr__(self):
+        return self._build_repr()
+
+    def _build_repr(self, prefix='', is_tail=True):
+        lines = []
+        node_label = f"{self.type}: {self.value}" if self.value is not None else f"{self.type}"
+        lines.append(prefix + ('└── ' if is_tail else '├── ') + node_label)
+        for i, child in enumerate(self.children):
+            next_prefix = prefix + ('    ' if is_tail else '│   ')
+            is_last = i == (len(self.children) - 1)
+            lines.append(child._build_repr(next_prefix, is_last))
+        return '\n'.join(lines)
--- parser/table.py
+++ parser/table.py
@@ -1,34 +1,32 @@
 ll1_table = {
     ('S', 'OPEN_PAREN'): ['Expr'],
-
     ('Expr', 'OPEN_PAREN'): ['OPEN_PAREN', 'RPNExpr', 'CLOSE_PAREN'],
 
     ('RPNExpr', 'OPEN_PAREN'): ['Operand', 'RPNExprPrime'],
     ('RPNExpr', 'NUMBER'): ['Operand', 'RPNExprPrime'],
-    ('RPNExpr', 'MEM'): ['Operand', 'RPNExprPrime'],
+    ('RPNExpr', 'MEM'): ['OperatorUnary'],
 
     ('RPNExprPrime', 'OPEN_PAREN'): ['Operand', 'Operator'],
     ('RPNExprPrime', 'NUMBER'): ['Operand', 'Operator'],
-    ('RPNExprPrime', 'MEM'): ['Operand', 'Operator'],
+    ('RPNExprPrime', 'MEM'): ['OperatorUnary'],
     ('RPNExprPrime', 'RES'): ['OperatorUnary'],
     ('RPNExprPrime', 'IF'): ['OperatorUnary'],
     ('RPNExprPrime', 'THEN'): ['OperatorUnary'],
     ('RPNExprPrime', 'ELSE'): ['OperatorUnary'],
     ('RPNExprPrime', 'FOR'): ['OperatorUnary'],
     ('RPNExprPrime', 'DO'): ['OperatorUnary'],
+    ('RPNExprPrime', 'CLOSE_PAREN'): [],
+    ('RPNExprPrime', '$'): [],
 
     ('Operand', 'OPEN_PAREN'): ['Expr'],
     ('Operand', 'NUMBER'): ['NUMBER'],
-    ('Operand', 'MEM'): ['MEM'],
 
     ('Operator', 'OPERATOR'): ['OPERATOR'],
-    ('OperatorUnary', 'RES'): ['RES'],
     ('OperatorUnary', 'MEM'): ['MEM'],
+    ('OperatorUnary', 'RES'): ['RES'],
     ('OperatorUnary', 'IF'): ['IF'],
     ('OperatorUnary', 'THEN'): ['THEN'],
     ('OperatorUnary', 'ELSE'): ['ELSE'],
     ('OperatorUnary', 'FOR'): ['FOR'],
     ('OperatorUnary', 'DO'): ['DO'],
-    ('RPNExprPrime','CLOSE_PAREN'): [],
-    ('RPNExprPrime','$'): [],
 }
